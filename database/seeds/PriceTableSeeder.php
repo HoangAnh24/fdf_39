306b8e52cd421758cdcc8f0ef7d66f01649f93c9
ngockieu96 <kieu65012@gmail.com>
2018-03-17 12:21:53 +0700

    seed task

---
+++ database/seeds/PriceTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Price;
+
+class PriceTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Price::class, 5)->create();
+    }
+}
